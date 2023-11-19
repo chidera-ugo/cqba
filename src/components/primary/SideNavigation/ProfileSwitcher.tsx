3075820ffcb6f1f4c1b4aa6cec2f487c3efb08bc
Chidera Ugo <chiderahugo@gmail.com>
2023-11-19 17:17:59 +0100

    Fix ui bug

--- src/components/primary/SideNavigation/ProfileSwitcher.tsx
+++ src/components/primary/SideNavigation/ProfileSwitcher.tsx
@@ -40,15 +40,15 @@ export const ProfileSwitcher = ({
           onClick={() => setShowDropdown((prev) => !prev)}
         >
           <div className='flex'>
-            <div className='y-center mr-2.5 h-12 w-12 rounded-full bg-white font-semibold uppercase'>
+            <div className='y-center mr-2.5 h-12 w-12 flex-shrink-0 rounded-full bg-white font-semibold uppercase'>
               {data?.businessName?.charAt(0)}
             </div>
 
             <div className='my-auto text-left'>
-              <div className='text-base font-semibold text-neutral-1000 line-clamp-1'>
+              <div className='break-all text-base font-semibold text-neutral-1000 line-clamp-1'>
                 {data?.businessName ?? '----'}
               </div>
-              <div className='text-xs text-neutral-600 line-clamp-1'>
+              <div className='break-all text-xs text-neutral-600 line-clamp-1'>
                 {user?.email}
               </div>
             </div>
