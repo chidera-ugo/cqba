3513ee158b99e88b62e0314278e0f662897fa1c0
Chidera Ugo <chiderahugo@gmail.com>
2023-12-22 09:41:37 +0100

    remove remember me

--- src/components/forms/auth/SignInForm/Form.tsx
+++ src/components/forms/auth/SignInForm/Form.tsx
@@ -1,7 +1,6 @@
 import { Input } from 'components/form-elements/Input';
 import { Form as FormikForm, FormikProps } from 'formik';
-import { useEffect } from 'react';
-import { getCookie, saveCookie } from 'utils/handlers/handleCookies';
+import { saveCookie } from 'utils/handlers/handleCookies';
 import { initialValues } from './initialValues';
 import { SubmitButton } from 'components/form-elements/SubmitButton';
 import { PasswordInput } from 'components/form-elements/PasswordInput';
@@ -14,14 +13,6 @@ interface Props {
 
 export const Form = ({ processing, formikProps }: Props) => {
   const { handleSubmit, values, setFieldValue } = formikProps;
-
-  useEffect(() => {
-    const rememberMe = getCookie('rememberMe');
-
-    if (!rememberMe || !rememberMe.value) return;
-
-    setFieldValue('rememberMe', rememberMe.value);
-  }, []);
 
   return (
     <FormikForm onSubmit={handleSubmit} className='mt-7'>
@@ -42,22 +33,24 @@ export const Form = ({ processing, formikProps }: Props) => {
       />
 
       <div className='x-between mt-4'>
-        <label
-          className='relative my-auto flex cursor-pointer gap-1.5'
-          htmlFor={'rememberMe'}
-        >
-          <input
-            type='checkbox'
-            checked={values.rememberMe}
-            onChange={() => {
-              setFieldValue('rememberMe', !values.rememberMe);
-              saveCookie('rememberMe', { value: !values.rememberMe });
-            }}
-            id='rememberMe'
-            className='my-auto'
-          />
-          <span className={'my-auto text-sm font-normal'}>Remember me</span>
-        </label>
+        <div>
+          <label
+            className='relative my-auto flex cursor-pointer gap-1.5'
+            htmlFor={'rememberMe'}
+          >
+            <input
+              type='checkbox'
+              checked={values.rememberMe}
+              onChange={() => {
+                setFieldValue('rememberMe', !values.rememberMe);
+                saveCookie('rememberMe', { value: !values.rememberMe });
+              }}
+              id='rememberMe'
+              className='my-auto'
+            />
+            <span className={'my-auto text-sm font-normal'}>Remember me</span>
+          </label>
+        </div>
 
         <Link
           href='/auth/forgot-password'
