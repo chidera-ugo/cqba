eb82520d57b22a8dfa04840959b9b3f52ef9f873
Chidera Ugo <chiderahugo@gmail.com>
2023-12-22 09:42:15 +0100

    remove remember me

--- src/components/forms/auth/SignInForm/Form.tsx
+++ src/components/forms/auth/SignInForm/Form.tsx
@@ -1,6 +1,5 @@
 import { Input } from 'components/form-elements/Input';
 import { Form as FormikForm, FormikProps } from 'formik';
-import { saveCookie } from 'utils/handlers/handleCookies';
 import { initialValues } from './initialValues';
 import { SubmitButton } from 'components/form-elements/SubmitButton';
 import { PasswordInput } from 'components/form-elements/PasswordInput';
@@ -12,7 +11,7 @@ interface Props {
 }
 
 export const Form = ({ processing, formikProps }: Props) => {
-  const { handleSubmit, values, setFieldValue } = formikProps;
+  const { handleSubmit } = formikProps;
 
   return (
     <FormikForm onSubmit={handleSubmit} className='mt-7'>
@@ -33,24 +32,7 @@ export const Form = ({ processing, formikProps }: Props) => {
       />
 
       <div className='x-between mt-4'>
-        <div>
-          <label
-            className='relative my-auto flex cursor-pointer gap-1.5'
-            htmlFor={'rememberMe'}
-          >
-            <input
-              type='checkbox'
-              checked={values.rememberMe}
-              onChange={() => {
-                setFieldValue('rememberMe', !values.rememberMe);
-                saveCookie('rememberMe', { value: !values.rememberMe });
-              }}
-              id='rememberMe'
-              className='my-auto'
-            />
-            <span className={'my-auto text-sm font-normal'}>Remember me</span>
-          </label>
-        </div>
+        <div></div>
 
         <Link
           href='/auth/forgot-password'
