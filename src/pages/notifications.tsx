e3fafb4d4aeef0bebe612e33ba1fcf7dfad995c7
Chidera Ugo <chiderahugo@gmail.com>
2024-01-03 15:21:24 +0100

    remove unused code

--- src/pages/notifications.tsx
+++
@@ -1,7 +0,0 @@
-import { AppLayout } from 'components/layouts/AppLayout';
-import NotFound from 'pages/404';
-
-export default function Notifications() {
-  if (!false) return <NotFound />;
-  return <AppLayout title='Notifications'></AppLayout>;
-}
