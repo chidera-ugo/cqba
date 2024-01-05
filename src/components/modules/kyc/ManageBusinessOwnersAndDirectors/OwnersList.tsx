cafcd73099d6c2ba7be87b39e4a3006dbdebf580
Chidera Ugo <chiderahugo@gmail.com>
2024-01-05 08:36:48 +0100

    Filter valid owners

--- src/components/modules/kyc/ManageBusinessOwnersAndDirectors/OwnersList.tsx
+++ src/components/modules/kyc/ManageBusinessOwnersAndDirectors/OwnersList.tsx
@@ -29,55 +29,57 @@ export const OwnersList = ({
 
   return (
     <>
-      {data?.map((owner) => {
-        const { firstName, lastName, percentOwned, _id } = owner;
+      {data
+        ?.filter((owner) => !!owner?.firstName)
+        .map((owner) => {
+          const { firstName, lastName, percentOwned, _id } = owner;
 
-        return (
-          <div
-            key={_id}
-            className='card mt-4 gap-2.5 rounded-2xl border-neutral-200 p-6'
-          >
-            <div className='text-left text-base font-medium text-black line-clamp-1 group-hover:text-primary-main'>
-              {firstName} {lastName}
-            </div>
-
-            <p className={'font-light text-neutral-500'}>Director, Owner</p>
-
-            <div className='x-between mt-4'>
-              <div
-                className={
-                  'y-center my-auto h-10 rounded-full border border-neutral-200 px-3 text-sm font-medium'
-                }
-              >
-                {percentOwned}% Ownership
+          return (
+            <div
+              key={_id}
+              className='card mt-4 gap-2.5 rounded-2xl border-neutral-200 p-6'
+            >
+              <div className='text-left text-base font-medium text-black line-clamp-1 group-hover:text-primary-main'>
+                {firstName} {lastName}
               </div>
 
-              <div className='flex gap-2'>
-                <button
-                  className={''}
-                  onClick={() => {
-                    setCurrentOwner(owner);
-                    handleShowModal();
-                  }}
-                >
-                  <EditIcon />
-                </button>
+              <p className={'font-light text-neutral-500'}>Director, Owner</p>
 
-                <button
-                  onClick={() => {
-                    setOwnerToDelete({
-                      name: `${firstName} ${lastName}`,
-                      id: _id,
-                    });
-                  }}
+              <div className='x-between mt-4'>
+                <div
+                  className={
+                    'y-center my-auto h-10 rounded-full border border-neutral-200 px-3 text-sm font-medium'
+                  }
                 >
-                  <DeleteIcon />
-                </button>
+                  {percentOwned}% Ownership
+                </div>
+
+                <div className='flex gap-2'>
+                  <button
+                    className={''}
+                    onClick={() => {
+                      setCurrentOwner(owner);
+                      handleShowModal();
+                    }}
+                  >
+                    <EditIcon />
+                  </button>
+
+                  <button
+                    onClick={() => {
+                      setOwnerToDelete({
+                        name: `${firstName} ${lastName}`,
+                        id: _id,
+                      });
+                    }}
+                  >
+                    <DeleteIcon />
+                  </button>
+                </div>
               </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })}
     </>
   );
 };
